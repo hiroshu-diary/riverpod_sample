51312629496ca85af633168063be1dd325623388
“hiroshu-diary” <hiroshu.diary@gmail.com>
2022-04-18 09:32:13 +0900

    UI修正

--- lib/mvvm/home_page.dart
+++ lib/mvvm/home_page.dart
@@ -10,17 +10,20 @@ class HomePage extends ConsumerWidget {
     final pageState = ref.watch(homePageNotifierProvider);
     final pageNotifier = ref.watch(homePageNotifierProvider.notifier);
     return Scaffold(
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Main Count ${pageState.mainCount}'),
-            onTap: pageNotifier.increaseMainCount,
-          ),
-          ListTile(
-            title: Text('Sub Count ${pageState.subCount}'),
-            onTap: pageNotifier.increaseSUbCount,
-          ),
-        ],
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(32),
+          children: [
+            ListTile(
+              title: Text('Main Count ${pageState.mainCount}'),
+              onTap: pageNotifier.increaseMainCount,
+            ),
+            ListTile(
+              title: Text('Sub Count ${pageState.subCount}'),
+              onTap: pageNotifier.increaseSUbCount,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: pageNotifier.resetAllCount,
